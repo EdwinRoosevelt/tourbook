b83998ba9ced16e2593d3a02a08b1cc603074e36
Edwin Roosevelt <bedwinroosevelt@gmail.com>
2022-06-15 22:40:28 +0530

    changed env variable names

---
+++ constants.js
@@ -0,0 +1,6 @@
+
+export const awsTourbookUser = {
+  accessKeyId: process.env.TOURBOOK_AWS_ACCESS_ID,
+  secretAccessKey: process.env.TOURBOOK_AWS_SECRET_KEY,
+  region: process.env.TOURBOOK_AWS_REGION,
+};
