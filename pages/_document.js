89930dc6c18cfbcd39cc229aedb32701c814d4a0
Edwin Roosevelt <bedwinroosevelt@gmail.com>
2022-06-23 13:36:04 +0530

    disabled blocking scripts

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -15,26 +16,27 @@ export default function Document() {
           crossOrigin="anonymous"
         ></link>
 
-        <script
-          async
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-          integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-          crossOrigin="anonymous"
-        ></script>
-
-        <script
+        {/* <script
           async
           src="https://www.gstatic.com/firebasejs/ui/6.0.1/firebase-ui-auth.js"
         ></script>
+
         <link
           type="text/css"
           rel="stylesheet"
           href="https://www.gstatic.com/firebasejs/ui/6.0.1/firebase-ui-auth.css"
-        />
+        /> */}
+        
       </Head>
       <body>
         <Main />
         <NextScript />
+        <Script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+          crossOrigin="anonymous"
+          strategy="beforeInteractive"
+        ></Script>
       </body>
     </Html>
   );
